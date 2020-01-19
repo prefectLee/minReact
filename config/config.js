0ce1bf902e17ffb4c84472185f000897c2e92adc
lee <394908996@qq.com>
2020-01-19 23:18:35 +0800

    更新

--- config/config.js
+++ config/config.js
@@ -1,3 +1,13 @@
 export default {
     singular: true,
+    plugins: [
+        ['umi-plugin-react', {
+            antd: true
+        }],
+    ],
+    routes: [{
+        path: '/',
+        component: './HelloWorld',
+    }],
+
 }
