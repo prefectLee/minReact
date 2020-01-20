d0fcbee03fcb10cba0f429144419592e8f16dd88
lee <394908996@qq.com>
2020-01-20 21:15:21 +0800

    20200120更新

--- config/config.js
+++ config/config.js
@@ -2,12 +2,35 @@ export default {
     singular: true,
     plugins: [
         ['umi-plugin-react', {
-            antd: true
+            antd: true,
+            dva: true,
         }],
     ],
     routes: [{
         path: '/',
-        component: './HelloWorld',
+        component: './layout',
+        routes: [
+            {
+                path: '/',
+                component: 'Helloworld',
+            },
+            {
+                path: '/helloworld',
+                component: 'Helloworld'
+            },
+            {
+                path: 'puzzlecards',
+                 component: './puzzlecards'
+            },
+            {
+                path: '/dashboard',
+                routes: [
+                    { path: '/dashboard/analysis', component: 'Dashboard/Analysis' },
+                    { path: '/dashboard/monitor', component: 'Dashboard/Monitor' },
+                    { path: '/dashboard/workplace', component: 'Dashboard/Workplace' }
+                ]
+            },
+        ]
     }],
 
 }
